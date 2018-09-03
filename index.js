3bb69d933dd567f88f2aa0f689e84a2feecd3ff6
StevenAprentisCodeur <41762297+StevenAprentisCodeur@users.noreply.github.com>
2018-09-03 20:44:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ bot.on('message', function (message) {
 	if(message.content === 'bn')
 		message.reply("Moi aussi j'aime les BN :troll: ")
 
-	if(message.content === '@Steven-ADev#1843') 
+	if(message.content === '@Steven-ADev') 
 		message.reply("Vous appeller le grand maitre. Patientez a un moment il viendra ....,");
 
 	
